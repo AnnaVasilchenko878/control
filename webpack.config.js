89692f963f9645bc19d93f7fc3a67bfaa85e3115
anna_vasilchenko <motolive2009@ramvler.ru>
2021-12-22 22:30:29 +0300

    Добавляет html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const PATH = require('path'),
 ISDEV = process.env.NODE_ENV === 'development',
 ISPROD = !ISDEV;
 const filename = (ext) =>ISDEV?`[name].${ext}`:`[name].[contenthash].${ext}`;
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     context: PATH.resolve(__dirname, 'src'),
     mode: 'development',
@@ -9,5 +10,15 @@ module.exports = {
     output: {
         filename: `./js/${filename('js')}`,
         path: PATH.resolve(__dirname, 'app')
-    }
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: PATH.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: ISPROD
+            }
+
+        })
+    ]
 };
